98e0113d35fcea57e55019b3ece75ad790af1ed6
Namita Malik <namita.malik@paytm.com>
2015-04-03 17:40:44 +0530

    Formatting done and folder structure corrected, HTML bug removed

--- src/controller/BookCntrl.js
+++ src/controller/BookCntrl.js
@@ -1,16 +1,35 @@
 /**
  * Created by namita on 13/3/15.
  */
-(function(ng){
+(function (ng) {
     var bookApp = ng.module('bookApp', []);
     bookApp.controller('BookCtrl', ["$scope", "BookService", function ($scope, BookService) {
         BookService.getBookResults()
             .success(function (response) {
-                $scope.book = response;
-                console.log($scope.book.books[0]);
+                $scope.book = response.books;
+                angular.forEach($scope.book,function(value,index){
+                    if($scope.book[index].librarysupported && $scope.book[index].librarysupported === 1){
+                        $scope.book[index].librarysupported = "Yes";
+                    }
+                    else if($scope.book[index].librarysupported === 0){
+                        $scope.book[index].librarysupported = "No"
+                    }
+                    else{
+                        $scope.book[index].librarysupported = "NA";
+                    }
+                });
+                console.log($scope.book[0].authors.firstname);
             })
             .error(function (status) {
                 console.log(status);
             });
+        $scope.reverse = false;
+        $scope.sortBy = function (field) {
+            if ($scope.orderByField == field) {
+                $scope.reverse = !$scope.reverse;
+            }
+            $scope.orderByField = field;
+        };
+
     }]);
 })(angular);
